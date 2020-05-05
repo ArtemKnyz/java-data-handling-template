049c9e47905c59b57af698aa059eb1c6298fc4b1
Artem K <artymk77@gmail.com>
2020-05-06 00:11:30 +0300

    add part of method's find simple count

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -9,6 +9,7 @@ public class SimpleBigNumbersService implements BigNumbersService {
     /**
      * Метод делит первое число на второе с заданной точностью
      * Например 1/3 с точностью 2 = 0.33
+     *
      * @param range точность
      * @return результат
      */
@@ -26,6 +27,18 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        return null;
+        BigInteger s = BigInteger.valueOf(0);
+
+        int sd = 1;
+        for (int i = 2; i < 100; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    System.out.println("это число непростое!" + i);
+                    break;
+                }
+            }
+
+        }
+        return BigInteger.valueOf(sd);
     }
 }
