11232b56d6769ad04db555755d3100a632d12bf5
Artem K <artymk77@gmail.com>
2020-05-09 21:55:14 +0300

    add new realise to maskSensitiveData methods

--- src/main/java/com/epam/izh/rd/online/service/SimpleDateService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleDateService.java
@@ -70,6 +70,4 @@ public class SimpleDateService implements DateService {
 
         return ChronoUnit.SECONDS.between(pastYear, nowYear);
     }
-
-
 }
--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
@@ -1,5 +1,10 @@
 package com.epam.izh.rd.online.service;
 
+import java.io.*;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleRegExpService implements RegExpService {
 
     /**
@@ -11,6 +16,30 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String maskSensitiveData() {
+        File file = new File(" ");
+
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            String st = br.readLine();
+            //System.out.println(st);
+
+
+            int i;
+            //Pattern pattern1 = Pattern.compile("\\d{4}-^\\d{4}-^\\d{4}-\\d{4}");
+            Pattern pattern1 = Pattern.compile("\\D+[\\d+\\s]{20}\\D+[\\d+\\s]{20}\\D+");
+            Pattern pattern = Pattern.compile("[0-9]+");
+            //Matcher matcher = pattern.matcher(st);
+            Matcher matcher = pattern1.matcher(st);
+            if (matcher.find()) {
+                System.out.println(matcher.group());
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 
